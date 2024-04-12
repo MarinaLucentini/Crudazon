a3b1e4adc77320b90dd72e7c9386561277d63a73
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-04-12 18:48:21 +0200

    fixed modificated

--- java/backoffice.js
+++ java/backoffice.js
@@ -5,6 +5,7 @@ const id = params.get("productId");
 const URL = id
   ? "https://striveschool-api.herokuapp.com/api/product/" + id
   : "https://striveschool-api.herokuapp.com/api/product/";
+const method = id ? "PUT" : "POST";
 
 const createNewProduct = (event) => {
   event.preventDefault();
@@ -16,9 +17,9 @@ const createNewProduct = (event) => {
     price: parseInt(document.getElementById("priceProduct").value),
   };
   form.reset();
-  console.log(newProduct);
+
   fetch(URL, {
-    method: "POST",
+    method: method,
     body: JSON.stringify(newProduct),
     headers: {
       "Content-Type": "application/json",
@@ -46,6 +47,7 @@ const resetForm = () => {
 btnReset.addEventListener("click", resetForm);
 
 window.addEventListener("DOMContentLoaded", () => {
+  form.addEventListener("submit", createNewProduct);
   if (id) {
     const title = document.getElementById("newOrMod");
     title.innerText = "Modifica prodotto";
@@ -55,38 +57,35 @@ window.addEventListener("DOMContentLoaded", () => {
     btnsubmit.classList.remove("btn-outline-success");
     const btnDelete = document.getElementById("deleteProduct");
     btnDelete.classList.remove("d-none");
-    form.addEventListener(
-      "submit",
-      fetch(URL, {
-        method: "PUT",
+    fetch(URL, {
+      method: method,
 
-        headers: {
-          "Content-Type": "application/json",
-
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4ZjYwYTdmMzA0NjAwMWFlNTlmYTEiLCJpYXQiOjE3MTI5MTE4ODIsImV4cCI6MTcxNDEyMTQ4Mn0.N1HwU5D3A4Ct3zgtwCEWlWzKHKjAauQfHbbMNuenJ4U",
-        },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4ZjYwYTdmMzA0NjAwMWFlNTlmYTEiLCJpYXQiOjE3MTI5MTE4ODIsImV4cCI6MTcxNDEyMTQ4Mn0.N1HwU5D3A4Ct3zgtwCEWlWzKHKjAauQfHbbMNuenJ4U",
+      },
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          throw new Error("Errore nella fetch");
+        }
       })
-        .then((resp) => {
-          if (resp.ok) {
-            return resp.json();
-          } else {
-            throw new Error("Errore nella fetch");
-          }
-        })
-        .then((productToMod) => {
-          const { name, description, price, imageUrl, brand } = productToMod;
-          document.getElementById("nameProduct").value = name;
-          document.getElementById("descriptionProduct").value = description;
-          document.getElementById("brandProduct").value = brand;
-          document.getElementById("imageProduct").value = imageUrl;
-          document.getElementById("priceProduct").value = price;
-        })
-        .catch((err) => console.log(err))
-    );
+      .then((productToMod) => {
+        console.log(productToMod);
+        const { name, description, price, imageUrl, brand } = productToMod;
+        document.getElementById("nameProduct").value = name;
+        document.getElementById("descriptionProduct").value = description;
+        document.getElementById("brandProduct").value = brand;
+        document.getElementById("imageProduct").value = imageUrl;
+        document.getElementById("priceProduct").value = price;
+        console.log(productToMod);
+      })
+      .catch((err) => console.log(err));
   } else {
     const title = document.getElementById("newOrMod");
     title.innerText = "Crea un nuovo prodotto";
-    form.addEventListener("submit", createNewProduct);
   }
 });
